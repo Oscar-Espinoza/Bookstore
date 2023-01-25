949b6b2999487c268a53e29ccfc3a6467856355e
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-25 14:53:55 -0300

    app ID added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
+export const apiId = 'qO00VEuARfSLWwqDGcKq';
+
 const store = configureStore({
   reducer: {
     books: booksReducer,
